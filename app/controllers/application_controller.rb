e6f6067cb39ac62c9bf5f266a78581afb486698d
elijahmm17 <elijahmm17@gmail.com>
2019-07-20 20:05:52 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,11 +18,11 @@ class ApplicationController < Sinatra::Base
 
   post "/signup" do
     #your code here
-    @username = params[:username]
-    @password = params[:password]
-    if @username.length > 0 && @password.length > 0
-      @user = User.create(username: @username, password: @password)
-      session[:user_id] = @user.id
+    username = params[:username]
+    password = params[:password]
+    if username.length > 0 && password.length > 0
+      user = User.create(username: username, password: password)
+      session[:user_id] = user.id
       redirect to '/login'
     else 
       redirect to '/failure'
